9b0d5599e088c740223a02ca4822363726d87ec1
CK <combizkhozoie@hotmail.com>
2018-09-21 15:14:00 +0100

    added readme

---
+++ .ipynb_checkpoints/ckshrna-checkpoint.py
@@ -0,0 +1,73 @@
+# CK SEPT 2017
+# PyQt5 GUI for shrna oligo design
+
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem
+from ckshrnagui import Ui_ckshrnagui
+
+class ckshrnagui(Ui_ckshrnagui):
+    def __init__(self, dialog):
+        Ui_ckshrnagui.__init__(self)
+        self.setupUi(dialog)
+
+        # Connect "add" button with a custom function (addInputTextToListbox)
+        self.generateOligosBtn.clicked.connect(self.beginOligoDesign)
+
+        self.undoBtn.clicked.connect(self.undoLastOligo)
+
+    def undoLastOligo(self):
+        currentRowCount = self.tableWidget.rowCount()  # necessary even when there are no rows in the table
+        self.tableWidget.removeRow(currentRowCount - 1)
+
+    def beginOligoDesign(self):
+        def reverse_complement(my_sequence):
+            my_dictionary = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+            return "".join([my_dictionary[base] for base in reversed(my_sequence)])
+
+        def complement(my_sequence):
+            my_dictionary = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+            return "".join([my_dictionary[base] for base in my_sequence])
+
+        def reverse(my_sequence):
+            return my_sequence[::-1]
+
+        shRNA_sense = self.inputSequence.text().upper()
+
+        # generate the 5' oligo
+        shRNA_antisense = reverse_complement(shRNA_sense)
+        loop_sequence = "CTCGAG"  # the 6bp loop CTCGAG or the 9bp loop TTCAAGAGA are good choices
+        sense_3 = "TTTTTG"  # 3' of the sense strand includes the ttttt pol iii termination sequence
+        sense_5 = "CCGG"  # 5' of the sense strand includes the AgeI overhang
+
+        sense_oligo = ''.join([sense_5.lower(), shRNA_sense, loop_sequence.lower(), shRNA_antisense, sense_3.lower()])
+
+        sense_label = "shRNA     sense 5'- " + sense_oligo + ' ' * 4 + " -3'"
+        self.senseLabel.setText(sense_label)
+
+        # and the complementary oligo
+        antisense_5 = "AATTCAAAAA"  # includes the EcoRI underhang and reverse complement of the term seq
+        antisense_oligo = ''.join([antisense_5.lower(), shRNA_sense, loop_sequence.lower(), shRNA_antisense])
+
+        antisense_label = "shRNA antisense 3'- " + ' ' * 4 + reverse(antisense_oligo) + " -5'"
+        self.antisenseLabel.setText(antisense_label)
+
+        # print the bonds
+        bonds_label = "                    " + ' ' * 4 + '|' * (len(antisense_oligo) - 4)
+        self.bondsLabel.setText(bonds_label)
+
+        currentRowCount = self.tableWidget.rowCount()  # necessary even when there are no rows in the table
+
+        self.tableWidget.insertRow(currentRowCount)
+        self.tableWidget.setItem(currentRowCount, 0, QTableWidgetItem(sense_oligo))
+        self.tableWidget.setItem(currentRowCount, 1, QTableWidgetItem(antisense_oligo))
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    dialog = QtWidgets.QDialog()
+
+    prog = ckshrnagui(dialog)
+
+    dialog.show()
+    sys.exit(app.exec_())
---
+++ README.md
@@ -0,0 +1,7 @@
+#### A simple GUI tool for design of oligonucleotides for cloning into a polIII vector for shRNA mediated gene knockdown
+##### by Combiz Khozoie, Ph.D.
+
+This tool was created for internal use within the laboratory to facilitate the design of oligonucleotides for shRNA gene knockdown.  The tool uses The RNA Consortium best practices for cloning into a 5' AgeI overhang  and 3' EcoRI underhang on the pLKO1-stuffer vector.
+
+The tool takes a 19-29bp stem sequence as input and returns two oligonucleotides ready to order in the 5'->3' orientation.
+
